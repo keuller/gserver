71594934c730734969c79d7b149aff708f2760d4
Claes Mogren <claes.mogren@accedo.tv>
2016-04-06 14:35:56 +0200

    Updated makefile and code cleanup

--- build-osx.sh
+++ build-osx.sh
@@ -3,4 +3,4 @@ export GOOS="darwin"
 export GOARCH="amd64"
 export VERSION=1.2.0
 export BUILD=`git rev-parse HEAD`
-go build -ldflags "-X main.Version=${VERSION} -X main.Build=${BUILD}" -o dist/gserver-osx gserver.go
+go build -ldflags "-X main.Version=${VERSION} -X main.Build=${BUILD}" -o ./bin/gserver-osx ./src/gserver.go
--- build-win.sh
+++ build-win.sh
@@ -4,4 +4,4 @@ export GOARCH="amd64"
 export VERSION=1.2.0
 export BUILD=`git rev-parse HEAD`
 
-go build -ldflags "-X main.Version=${VERSION} -X main.Build=${BUILD}" -o dist/gserver.exe gserver.go
+go build -ldflags "-X main.Version=${VERSION} -X main.Build=${BUILD}" -o ./bin/gserver.exe ./src/gserver.go
